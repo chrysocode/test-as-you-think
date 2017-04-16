5d2fbbf4b80b70ba4ec270e9eefcbdcc067718f8
Xavier Pigeon <xpigeon@wemanity.com>
2017-04-17 00:30:06 +0200

    Verify each step has been called in the right order by using a strict mock.

--- given-when-then-core/src/test/java/givenwhenthen/GivenWhenThenTest.java
+++ given-when-then-core/src/test/java/givenwhenthen/GivenWhenThenTest.java
@@ -2,10 +2,11 @@ package givenwhenthen;
 
 import static givenwhenthen.GivenWhenThen.givenSut;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.easymock.EasyMock.mock;
 import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.strictMock;
 import static org.easymock.EasyMock.verify;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,16 +17,22 @@ public class GivenWhenThenTest {
 
     @Before
     public void prepareFixtures() {
-        sut = new SystemUnderTest();
-
-        givenWhenThenDefinitionMock = mock(GivenWhenThenDefinition.class);
+        givenWhenThenDefinitionMock = strictMock(GivenWhenThenDefinition.class);
         givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
+        givenWhenThenDefinitionMock.whenAnEventHappensInRelationToAnActionOfTheConsumer();
+        givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
         replay(givenWhenThenDefinitionMock);
+
+        sut = new SystemUnderTest(givenWhenThenDefinitionMock);
+    }
+
+    @After
+    public void verifyMocks() {
+        verify(givenWhenThenDefinitionMock);
     }
 
     @Test
     public void should_follow_the_given_when_then_full_sequence_given_a_non_void_method() {
-        // WHEN
         givenSut(sut) //
                 .given(() -> {
                     givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
@@ -35,15 +42,12 @@ public class GivenWhenThenTest {
                 }) //
                 .then(result -> {
                     assertThat(result).isEqualTo("expected result");
+                    givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
                 });
-
-        // THEN
-        verify(givenWhenThenDefinitionMock);
     }
 
     @Test
     public void should_follow_the_given_when_then_full_sequence_given_a_void_method() {
-        // WHEN
         givenSut(sut) //
                 .given(() -> {
                     givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
@@ -52,10 +56,7 @@ public class GivenWhenThenTest {
                     sut.voidMethod();
                 }) //
                 .then((Void) -> {
-                    assertThat(sut.getVoidMethodResult()).isEqualTo("expected result");
+                    givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
                 });
-
-        // THEN
-        verify(givenWhenThenDefinitionMock);
     }
 }
--- given-when-then-core/src/test/java/givenwhenthen/SystemUnderTest.java
+++ given-when-then-core/src/test/java/givenwhenthen/SystemUnderTest.java
@@ -2,17 +2,18 @@ package givenwhenthen;
 
 public class SystemUnderTest {
 
-    private String voidMethodResult;
+    private GivenWhenThenDefinition givenWhenThenDefinition;
+
+    public SystemUnderTest(GivenWhenThenDefinition givenWhenThenDefinition) {
+        this.givenWhenThenDefinition = givenWhenThenDefinition;
+    }
 
     public String nonVoidMethod() {
+        givenWhenThenDefinition.whenAnEventHappensInRelationToAnActionOfTheConsumer();
         return "expected result";
     }
 
     public void voidMethod() {
-        voidMethodResult = "expected result";
-    }
-
-    public String getVoidMethodResult() {
-        return voidMethodResult;
+        givenWhenThenDefinition.whenAnEventHappensInRelationToAnActionOfTheConsumer();
     }
 }
