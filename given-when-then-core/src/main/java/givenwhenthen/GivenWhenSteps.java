6d4e589fae236691ab216e364227efb18c8feb93
Xavier Pigeon <xpn.projects@gmail.com>
2017-05-18 01:52:19 +0200

    Refactor to deduplicate code.
    - Generalize the then step creation based on the use of the dedicated factory, in all remaining then methods.

--- given-when-then-core/src/main/java/givenwhenthen/GivenWhenSteps.java
+++ given-when-then-core/src/main/java/givenwhenthen/GivenWhenSteps.java
@@ -17,6 +17,7 @@ public class GivenWhenSteps<$SystemUnderTest> implements Given<$SystemUnderTest>
 
     private final Functions functions = Functions.INSTANCE;
     private final Preparation<$SystemUnderTest> preparation;
+    private final ThenStepFactory thenStepFactory = ThenStepFactory.INSTANCE;
 
     GivenWhenSteps($SystemUnderTest systemUnderTest) {
         preparation = new Preparation<>(systemUnderTest);
@@ -70,31 +71,16 @@ public class GivenWhenSteps<$SystemUnderTest> implements Given<$SystemUnderTest>
 
     @Override
     public <$Result> Then<$SystemUnderTest, $Result> when(CheckedFunction<$SystemUnderTest, $Result> whenStep) {
-        return toThenStep(whenStep);
-    }
-
-    private <$Result> Then<$SystemUnderTest, $Result> toThenStep(CheckedFunction<$SystemUnderTest, $Result> whenStep) {
-        Event<$SystemUnderTest, $Result> event = new Event<>(preparation.getSystemUnderTest(), whenStep);
-        GivenWhenContext<$SystemUnderTest, $Result> context = new GivenWhenContext<>(preparation, event);
-        return new ThenStep<>(context);
+        return thenStepFactory.createThenStep(preparation, whenStep);
     }
 
     @Override
     public ThenWithoutResult<$SystemUnderTest> when(CheckedConsumer<$SystemUnderTest> whenStep) {
-        return toThenStep(whenStep);
-    }
-
-    private ThenWithoutResult<$SystemUnderTest> toThenStep(CheckedConsumer<$SystemUnderTest> whenStep) {
-        Event<$SystemUnderTest, Void> event = new Event<>(preparation.getSystemUnderTest(), whenStep);
-        GivenWhenContext<$SystemUnderTest, Void> context = new GivenWhenContext<>(preparation, event);
-        return new ThenWithoutResultStep<>(context);
+        return thenStepFactory.createThenStep(preparation, whenStep);
     }
 
     @Override
     public ThenFailure whenSutRunsOutsideOperatingConditions(CheckedConsumer<$SystemUnderTest> whenStep) {
-        Event<$SystemUnderTest, Throwable> event = new Event<>(preparation.getSystemUnderTest(),
-                functions.toFunctionWithThrowableAsResult(whenStep));
-        GivenWhenContext<$SystemUnderTest, Throwable> context = new GivenWhenContext<>(preparation, event);
-        return new ThenStep<>(context);
+        return thenStepFactory.createThenStep(preparation, functions.toFunctionWithThrowableAsResult(whenStep));
     }
 }
