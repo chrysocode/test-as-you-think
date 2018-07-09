e07a14c596cc7ea4f9c4627da9f51bdff803e6bc
Xavier Pigeon <xpn.projects@gmail.com>
2018-07-09 23:59:59 +0200

    Refactor tests to nest them in a test case tree.

--- test-as-you-think-core/src/test/java/testasyouthink/ThenExpectedFailuresTest.java
+++ test-as-you-think-core/src/test/java/testasyouthink/ThenExpectedFailuresTest.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -24,6 +24,7 @@ package testasyouthink;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,172 +60,189 @@ class ThenExpectedFailuresTest {
         verify(systemUnderTestMock);
     }
 
-    @Test
-    void should_verify_the_sut_fails() throws Throwable {
-        // GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException());
-        replay(systemUnderTestMock);
+    @Nested
+    class Then_verifying_the_failure {
 
-        // WHEN
-        givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails();
+        @Test
+        void should_verify_the_sut_fails() throws Throwable {
+            // GIVEN
+            expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException());
+            replay(systemUnderTestMock);
+
+            // WHEN
+            givenSut(systemUnderTestMock)
+                    .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                    .thenItFails();
+        }
+
+        @Test
+        void should_fail_given_a_missing_failure() throws Throwable {
+            // GIVEN
+            expect(systemUnderTestMock.methodWithThrowsClause()).andReturn("ordinary result");
+            replay(systemUnderTestMock);
+
+            // WHEN
+            Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
+                    .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                    .thenItFails());
+
+            // THEN
+            LOGGER.debug("Stack trace", thrown);
+            assertThat(thrown)
+                    .isInstanceOf(AssertionError.class)
+                    .hasMessage("Expecting a failure, but it was missing.");
+        }
+
+        @Test
+        void should_fail_given_an_unexpected_exception() throws Throwable {
+            // GIVEN
+            expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new UnexpectedException());
+            replay(systemUnderTestMock);
+
+            // WHEN
+            Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
+                    .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                    .thenItFails()
+                    .becauseOf(ExpectedException.class));
+
+            // THEN
+            LOGGER.debug("Stack trace", thrown);
+            assertThat(thrown).isInstanceOf(AssertionError.class);
+        }
+
+        @Nested
+        class Then_verifying_the_message_of_the_failure {
+
+            @Test
+            void should_verify_the_sut_fails_by_raising_an_expected_exception() throws Throwable {
+                // GIVEN
+                expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException(EXPECTED_MESSAGE));
+                replay(systemUnderTestMock);
+
+                // WHEN
+                givenSut(systemUnderTestMock)
+                        .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                        .thenItFails()
+                        .becauseOf(ExpectedException.class)
+                        .withMessage(EXPECTED_MESSAGE);
+            }
+
+            @Test
+            void should_verify_the_sut_fails_by_raising_an_expected_exception_with_an_expected_message() throws
+                    Throwable {
+                // GIVEN
+                expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException(EXPECTED_MESSAGE));
+                replay(systemUnderTestMock);
+
+                // WHEN
+                givenSut(systemUnderTestMock)
+                        .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                        .thenItFails()
+                        .becauseOf(ExpectedException.class)
+                        .withMessage(EXPECTED_MESSAGE);
+            }
+
+            @Test
+            void should_fail_given_an_unexpected_message() throws Throwable {
+                // GIVEN
+                expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
+                        new ExpectedException(UNEXPECTED_MESSAGE));
+                replay(systemUnderTestMock);
+
+                // WHEN
+                Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
+                        .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                        .thenItFails()
+                        .becauseOf(ExpectedException.class)
+                        .withMessage(EXPECTED_MESSAGE));
+
+                // THEN
+                LOGGER.debug("Stack trace", thrown);
+                assertThat(thrown).isInstanceOf(AssertionError.class);
+            }
+        }
     }
 
-    @Test
-    void should_verify_the_sut_fails_by_raising_an_expected_exception() throws Throwable {
-        // GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException(EXPECTED_MESSAGE));
-        replay(systemUnderTestMock);
+    @Nested
+    class Then_verifying_the_cause_of_the_failure {
 
-        // WHEN
-        givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE);
-    }
+        @Test
+        void should_verify_the_cause_of_a_failure() throws Throwable {
+            //GIVEN
+            expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
+                    new ExpectedException(EXPECTED_MESSAGE, new ExpectedException()));
+            replay(systemUnderTestMock);
 
-    @Test
-    void should_fail_given_a_missing_failure() throws Throwable {
-        // GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andReturn("ordinary result");
-        replay(systemUnderTestMock);
+            // WHEN
+            givenSut(systemUnderTestMock)
+                    .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                    .thenItFails()
+                    .becauseOf(ExpectedException.class)
+                    .withMessage(EXPECTED_MESSAGE)
+                    .havingCause(ExpectedException.class);
+        }
 
-        // WHEN
-        Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails());
+        @Test
+        void should_fail_to_verify_the_cause_of_a_failure_given_an_unexpected_cause() throws Throwable {
+            //GIVEN
+            expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
+                    new ExpectedException(EXPECTED_MESSAGE, new UnexpectedException()));
+            replay(systemUnderTestMock);
 
-        // THEN
-        LOGGER.debug("Stack trace", thrown);
-        assertThat(thrown)
-                .isInstanceOf(AssertionError.class)
-                .hasMessage("Expecting a failure, but it was missing.");
-    }
+            // WHEN
+            Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
+                    .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                    .thenItFails()
+                    .becauseOf(ExpectedException.class)
+                    .withMessage(EXPECTED_MESSAGE)
+                    .havingCause(ExpectedException.class));
 
-    @Test
-    void should_fail_given_an_unexpected_exception() throws Throwable {
-        // GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new UnexpectedException());
-        replay(systemUnderTestMock);
+            // THEN
+            LOGGER.debug("Stack trace", thrown);
+            assertThat(thrown).isInstanceOf(AssertionError.class);
+        }
 
-        // WHEN
-        Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class));
+        @Nested
+        class Then_verifying_the_cause_message_of_the_failure {
 
-        // THEN
-        LOGGER.debug("Stack trace", thrown);
-        assertThat(thrown).isInstanceOf(AssertionError.class);
-    }
+            @Test
+            void should_verify_the_cause_message_of_a_failure() throws Throwable {
+                //GIVEN
+                expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
+                        new ExpectedException(EXPECTED_MESSAGE, new ExpectedException(EXPECTED_CAUSE_MESSAGE)));
+                replay(systemUnderTestMock);
 
-    @Test
-    void should_verify_the_sut_fails_by_raising_an_expected_exception_with_an_expected_message() throws
-            Throwable {
-        // GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException(EXPECTED_MESSAGE));
-        replay(systemUnderTestMock);
+                // WHEN
+                givenSut(systemUnderTestMock)
+                        .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                        .thenItFails()
+                        .becauseOf(ExpectedException.class)
+                        .withMessage(EXPECTED_MESSAGE)
+                        .havingCause(ExpectedException.class)
+                        .withCauseMessage(EXPECTED_CAUSE_MESSAGE);
+            }
 
-        // WHEN
-        givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE);
-    }
+            @Test
+            void should_fail_to_verify_the_cause_message_of_a_failure_given_an_unexpected_cause_message() throws
+                    Throwable {
+                //GIVEN
+                expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
+                        new ExpectedException(EXPECTED_MESSAGE, new ExpectedException("unexpected cause message")));
+                replay(systemUnderTestMock);
 
-    @Test
-    void should_fail_given_an_unexpected_message() throws Throwable {
-        // GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(new ExpectedException(UNEXPECTED_MESSAGE));
-        replay(systemUnderTestMock);
+                // WHEN
+                Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
+                        .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
+                        .thenItFails()
+                        .becauseOf(ExpectedException.class)
+                        .withMessage(EXPECTED_MESSAGE)
+                        .havingCause(ExpectedException.class)
+                        .withCauseMessage(EXPECTED_CAUSE_MESSAGE));
 
-        // WHEN
-        Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE));
-
-        // THEN
-        LOGGER.debug("Stack trace", thrown);
-        assertThat(thrown).isInstanceOf(AssertionError.class);
-    }
-
-    @Test
-    void should_verify_the_cause_of_a_failure() throws Throwable {
-        //GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
-                new ExpectedException(EXPECTED_MESSAGE, new ExpectedException()));
-        replay(systemUnderTestMock);
-
-        // WHEN
-        givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE)
-                .havingCause(ExpectedException.class);
-    }
-
-    @Test
-    void should_fail_to_verify_the_cause_of_a_failure_given_an_unexpected_cause() throws Throwable {
-        //GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
-                new ExpectedException(EXPECTED_MESSAGE, new UnexpectedException()));
-        replay(systemUnderTestMock);
-
-        // WHEN
-        Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE)
-                .havingCause(ExpectedException.class));
-
-        // THEN
-        LOGGER.debug("Stack trace", thrown);
-        assertThat(thrown).isInstanceOf(AssertionError.class);
-    }
-
-    @Test
-    void should_verify_the_cause_message_of_a_failure() throws Throwable {
-        //GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
-                new ExpectedException(EXPECTED_MESSAGE, new ExpectedException(EXPECTED_CAUSE_MESSAGE)));
-        replay(systemUnderTestMock);
-
-        // WHEN
-        givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE)
-                .havingCause(ExpectedException.class)
-                .withCauseMessage(EXPECTED_CAUSE_MESSAGE);
-    }
-
-    @Test
-    void should_fail_to_verify_the_cause_message_of_a_failure_given_an_unexpected_cause_message() throws
-            Throwable {
-        //GIVEN
-        expect(systemUnderTestMock.methodWithThrowsClause()).andThrow(
-                new ExpectedException(EXPECTED_MESSAGE, new ExpectedException("unexpected cause message")));
-        replay(systemUnderTestMock);
-
-        // WHEN
-        Throwable thrown = catchThrowable(() -> givenSut(systemUnderTestMock)
-                .whenSutRunsOutsideOperatingConditions(SystemUnderTest::methodWithThrowsClause)
-                .thenItFails()
-                .becauseOf(ExpectedException.class)
-                .withMessage(EXPECTED_MESSAGE)
-                .havingCause(ExpectedException.class)
-                .withCauseMessage(EXPECTED_CAUSE_MESSAGE));
-
-        // THEN
-        LOGGER.debug("Stack trace", thrown);
-        assertThat(thrown).isInstanceOf(AssertionError.class);
+                // THEN
+                LOGGER.debug("Stack trace", thrown);
+                assertThat(thrown).isInstanceOf(AssertionError.class);
+            }
+        }
     }
 }
