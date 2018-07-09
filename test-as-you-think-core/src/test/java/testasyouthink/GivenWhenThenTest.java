d200eb90698de86728f027b091689683ec96eb4e
Xavier Pigeon <xpn.projects@gmail.com>
2018-07-10 00:44:04 +0200

    Refactor tests to nest them in a test case tree.

--- test-as-you-think-core/src/test/java/testasyouthink/GivenWhenThenTest.java
+++ test-as-you-think-core/src/test/java/testasyouthink/GivenWhenThenTest.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -23,6 +23,7 @@
 package testasyouthink;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import testasyouthink.fixture.GivenWhenThenDefinition;
 import testasyouthink.fixture.SystemUnderTest;
@@ -42,69 +43,6 @@ class GivenWhenThenTest {
     void verifyMocks() {
         // THEN
         verify(givenWhenThenDefinitionMock);
-    }
-
-    @Test
-    void should_follow_the_given_when_then_full_sequence_given_a_non_void_method() {
-        // GIVEN
-        givenWhenThenDefinitionMock = orderedSteps();
-
-        // WHEN
-        givenSut(new SystemUnderTest(givenWhenThenDefinitionMock))
-                .given(() -> givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem())
-                .when(SystemUnderTest::nonVoidMethod)
-                .then(result -> {
-                    givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
-                    assertThat(result).isEqualTo(EXPECTED_RESULT);
-                });
-    }
-
-    @Test
-    void should_follow_the_given_when_then_full_sequence_given_a_void_method() {
-        // GIVEN
-        givenWhenThenDefinitionMock = orderedSteps();
-
-        // WHEN
-        givenSut(new SystemUnderTest(givenWhenThenDefinitionMock))
-                .given(() -> givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem())
-                .when(SystemUnderTest::voidMethod)
-                .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
-    }
-
-    @Test
-    void should_follow_the_given_when_then_full_sequence_given_a_sut_class_to_be_instantiated() {
-        // GIVEN
-        givenWhenThenDefinitionMock = orderedSteps();
-
-        // WHEN
-        givenSutClass(SystemUnderTest.class)
-                .given(sut -> {
-                    givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
-                    sut.setGivenWhenThenDefinition(givenWhenThenDefinitionMock);
-                })
-                .when(SystemUnderTest::nonVoidMethod)
-                .then(result -> {
-                    givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
-                    assertThat(result).isEqualTo(EXPECTED_RESULT);
-                });
-    }
-
-    @Test
-    void should_verify_expectations_on_the_sut_given_a_void_method() {
-        // GIVEN
-        givenWhenThenDefinitionMock = orderedSteps();
-
-        // WHEN
-        givenSutClass(SystemUnderTest.class)
-                .given(sut -> {
-                    givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
-                    sut.setGivenWhenThenDefinition(givenWhenThenDefinitionMock);
-                })
-                .when(SystemUnderTest::voidMethod)
-                .then(sut -> {
-                    givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
-                    assertThat(sut.getState()).isNotNull();
-                });
     }
 
     @Test
@@ -128,32 +66,149 @@ class GivenWhenThenTest {
                 });
     }
 
-    @Test
-    void should_prepare_the_sut_with_a_given_step() {
-        // GIVEN
-        givenWhenThenDefinitionMock = orderedSteps();
+    @Nested
+    class Given_a_SUT_as_an_instance {
 
-        // WHEN
-        givenSut(() -> {
-            SystemUnderTest systemUnderTest = new SystemUnderTest(givenWhenThenDefinitionMock);
-            givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
-            return systemUnderTest;
-        })
-                .when(SystemUnderTest::voidMethod)
-                .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
+        @Test
+        void should_follow_the_when_then_partial_sequence_given_a_void_method() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps(0, 1, 1);
+
+            // WHEN
+            givenSut(new SystemUnderTest(givenWhenThenDefinitionMock))
+                    .when(SystemUnderTest::voidMethod)
+                    .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
+        }
+
+        @Test
+        void should_follow_the_when_then_partial_sequence_given_a_non_void_method() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps(0, 1, 1);
+
+            // WHEN
+            givenSut(new SystemUnderTest(givenWhenThenDefinitionMock))
+                    .when(SystemUnderTest::nonVoidMethod)
+                    .then(result -> {
+                        givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
+                        assertThat(result).isEqualTo(EXPECTED_RESULT);
+                    });
+        }
+
+        @Test
+        void should_follow_the_given_when_then_full_sequence_given_a_non_void_method() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps();
+
+            // WHEN
+            givenSut(new SystemUnderTest(givenWhenThenDefinitionMock))
+                    .given(() -> givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem())
+                    .when(SystemUnderTest::nonVoidMethod)
+                    .then(result -> {
+                        givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
+                        assertThat(result).isEqualTo(EXPECTED_RESULT);
+                    });
+        }
+
+        @Test
+        void should_follow_the_given_when_then_full_sequence_given_a_void_method() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps();
+
+            // WHEN
+            givenSut(new SystemUnderTest(givenWhenThenDefinitionMock))
+                    .given(() -> givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem())
+                    .when(SystemUnderTest::voidMethod)
+                    .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
+        }
     }
 
-    @Test
-    void should_prepare_the_sut_with_a_given_step_given_a_sut_class_to_be_instantiated() {
-        // GIVEN
-        givenWhenThenDefinitionMock = orderedSteps();
+    @Nested
+    class Given_a_SUT_as_a_class {
 
-        // WHEN
-        givenSut(SystemUnderTest.class, sut -> {
-            sut.setGivenWhenThenDefinition(givenWhenThenDefinitionMock);
-            givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
-        })
-                .when(SystemUnderTest::voidMethod)
-                .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
+        @Test
+        void should_follow_the_when_then_partial_sequence_given_a_sut_class_to_be_instantiated() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps(0, 1, 1);
+
+            // WHEN
+            givenSutClass(SystemUnderTest.class)
+                    .when(sut -> {
+                        givenWhenThenDefinitionMock.whenAnEventHappensInRelationToAnActionOfTheConsumer();
+                        return sut.nonVoidMethod();
+                    })
+                    .then(result -> {
+                        givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
+                        assertThat(result).isEqualTo(EXPECTED_RESULT);
+                    });
+        }
+    }
+
+    @Nested
+    class Given_a_SUT_to_be_prepared {
+
+        @Test
+        void should_follow_the_given_when_then_full_sequence_given_a_sut_class_to_be_instantiated() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps();
+
+            // WHEN
+            givenSutClass(SystemUnderTest.class)
+                    .given(sut -> {
+                        givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
+                        sut.setGivenWhenThenDefinition(givenWhenThenDefinitionMock);
+                    })
+                    .when(SystemUnderTest::nonVoidMethod)
+                    .then(result -> {
+                        givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
+                        assertThat(result).isEqualTo(EXPECTED_RESULT);
+                    });
+        }
+
+        @Test
+        void should_verify_expectations_on_the_sut_given_a_void_method() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps();
+
+            // WHEN
+            givenSutClass(SystemUnderTest.class)
+                    .given(sut -> {
+                        givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
+                        sut.setGivenWhenThenDefinition(givenWhenThenDefinitionMock);
+                    })
+                    .when(SystemUnderTest::voidMethod)
+                    .then(sut -> {
+                        givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
+                        assertThat(sut.getState()).isNotNull();
+                    });
+        }
+
+        @Test
+        void should_prepare_the_sut_with_a_given_step() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps();
+
+            // WHEN
+            givenSut(() -> {
+                SystemUnderTest systemUnderTest = new SystemUnderTest(givenWhenThenDefinitionMock);
+                givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
+                return systemUnderTest;
+            })
+                    .when(SystemUnderTest::voidMethod)
+                    .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
+        }
+
+        @Test
+        void should_prepare_the_sut_with_a_given_step_given_a_sut_class_to_be_instantiated() {
+            // GIVEN
+            givenWhenThenDefinitionMock = orderedSteps();
+
+            // WHEN
+            givenSut(SystemUnderTest.class, sut -> {
+                sut.setGivenWhenThenDefinition(givenWhenThenDefinitionMock);
+                givenWhenThenDefinitionMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
+            })
+                    .when(SystemUnderTest::voidMethod)
+                    .then(() -> givenWhenThenDefinitionMock.thenTheActualResultIsInKeepingWithTheExpectedResult());
+        }
     }
 }
