8ae58368b85935b113ded29f61d0bdb9ae0cb800
Xavier Pigeon <xpn.projects@gmail.com>
2018-07-15 18:52:43 +0200

    Refactor tests to nest them in a test case tree.

--- test-as-you-think-core/src/test/java/testasyouthink/ThenNoFailureTest.java
+++ test-as-you-think-core/src/test/java/testasyouthink/ThenNoFailureTest.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -26,9 +26,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import testasyouthink.fixture.GivenWhenThenDefinition;
 import testasyouthink.fixture.SystemUnderTest;
+import testasyouthink.fixture.UnexpectedException;
+import testasyouthink.function.CheckedRunnable;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static testasyouthink.TestAsYouThink.givenSutClass;
@@ -36,6 +42,7 @@ import static testasyouthink.TestAsYouThink.when;
 
 class ThenNoFailureTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThenNoFailureTest.class);
     private GivenWhenThenDefinition gwtMock;
 
     @BeforeEach
@@ -62,5 +69,20 @@ class ThenNoFailureTest {
     void should_verify_no_failure_happened_during_the_execution_stage_while_starting_with_the_event() {
         // WHEN
         when(() -> gwtMock.whenAnEventHappensInRelationToAnActionOfTheConsumer()).thenItSucceeds();
+    }
+
+    @Test
+    void should_fail_to_verify_no_failure_happened_during_the_execution_stage() {
+        // WHEN
+        Throwable thrown = catchThrowable(() -> when((CheckedRunnable) () -> {
+            gwtMock.whenAnEventHappensInRelationToAnActionOfTheConsumer();
+            throw new UnexpectedException();
+        }).thenItSucceeds());
+
+        // THEN
+        LOGGER.debug("Stack trace", thrown);
+        assertThat(thrown)
+                .isInstanceOf(AssertionError.class)
+                .hasMessageContaining(UnexpectedException.class.getName());
     }
 }
