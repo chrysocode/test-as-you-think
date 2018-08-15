98b91fdf5964458a82a3e70a5259bbe271f71409
Xavier Pigeon <xpn.projects@gmail.com>
2018-08-15 20:10:26 +0200

    It should prepare stdin to read messages.

--- test-as-you-think-core/src/test/java/testasyouthink/GivenStdinAsFixtureTest.java
+++ test-as-you-think-core/src/test/java/testasyouthink/GivenStdinAsFixtureTest.java
@@ -14,8 +14,12 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.inOrder;
 import static testasyouthink.TestAsYouThink.givenSutClass;
@@ -46,6 +50,54 @@ class GivenStdinAsFixtureTest {
                 })
                 .then(result -> {
                     assertThat(result).isEqualTo("expected");
+                    gwtMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
+                });
+
+        // THEN
+        InOrder inOrder = inOrder(gwtMock);
+        inOrder
+                .verify(gwtMock)
+                .givenAContextThatDefinesTheInitialStateOfTheSystem();
+        inOrder
+                .verify(gwtMock)
+                .whenAnEventHappensInRelationToAnActionOfTheConsumer();
+        inOrder
+                .verify(gwtMock)
+                .thenTheActualResultIsInKeepingWithTheExpectedResult();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void should_prepare_stdin_to_read_messages() {
+        // GIVEN
+        GivenWhenThenDefinition gwtMock = Mockito.mock(GivenWhenThenDefinition.class);
+
+        // WHEN
+        givenSutClass(SystemUnderTest.class)
+                .given(() -> {
+                    List<String> givenInputMessages = asList("intput #1", "intput #2", "intput #3");
+                    InputStream stdinFake = new ByteArrayInputStream(givenInputMessages
+                            .stream()
+                            .collect(Collectors.joining("\n"))
+                            .getBytes());
+                    System.setIn(stdinFake);
+                    gwtMock.givenAContextThatDefinesTheInitialStateOfTheSystem();
+                })
+                .when(sut -> {
+                    System.out.print("Type: ");
+                    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+                    List<String> actualMessages = new ArrayList<>();
+                    while (stdin.ready()) {
+                        String actualMessage = stdin.readLine();
+                        System.out.println(String.format("\nMessage: %s", actualMessage));
+                        actualMessages.add(actualMessage);
+                    }
+                    stdin.close();
+                    gwtMock.whenAnEventHappensInRelationToAnActionOfTheConsumer();
+                    return actualMessages;
+                })
+                .then(result -> {
+                    assertThat(result).containsExactly("intput #1", "intput #2", "intput #3");
                     gwtMock.thenTheActualResultIsInKeepingWithTheExpectedResult();
                 });
 
